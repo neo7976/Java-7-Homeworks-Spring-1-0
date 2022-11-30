3f73dcd9f4272b7ab66cad16f324098fa7816880
Dmitrii Sobin <neo7976@yandex.ru>
2022-11-30 12:47:38 +0300

    Разбор REST

--- src/main/java/ru/sobinda/springlesson/controller/HelloController.java
+++ src/main/java/ru/sobinda/springlesson/controller/HelloController.java
@@ -1,8 +1,8 @@
 package ru.sobinda.springlesson.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.sobinda.springlesson.domain.Person;
 
 //@RestController
 //@ConfigurationProperties("hello")
@@ -25,8 +25,24 @@ public class HelloController {
     private String from;
 
 
-    @GetMapping("/")
-    private String hello() {
-        return String.format("Hello %s", from);
+//    @GetMapping("/")
+//    private String hello() {
+//        return String.format("Hello %s", from);
+//    }
+
+    @PostMapping("/hello")
+    private String hello(@RequestBody Person guest) {
+        return String.format("Hello from %s to name %s age %d", from, guest.getName(), guest.getAge());
+    }
+
+    @GetMapping("/helloGet")
+    private String helloGet(Person guest) {
+        return String.format("Hello from %s to name %s age %d", from, guest.getName(), guest.getAge());
+    }
+
+    @GetMapping("/helloGet2")
+    private String helloGet2(@RequestParam("name") String name,
+                             @RequestParam("age") int age) {
+        return String.format("Hello from %s to name %s age %d", from, name, age);
     }
 }
